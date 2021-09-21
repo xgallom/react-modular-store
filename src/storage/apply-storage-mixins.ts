a138edb7a7ced5a7a58b0593c51ef4c54b6484c3
Milan Gallo <gallo.milan.jr@gmail.com>
2021-09-21 12:29:34 +0200

    Clap the call
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- src/storage/apply-storage-mixins.ts
+++ src/storage/apply-storage-mixins.ts
@@ -1,7 +1,11 @@
 import {BaseStorage, BaseStorageMixin, Storage} from './interface';
 
-export const applyStorageMixins = (mixins: BaseStorageMixin[], storage: BaseStorage) =>
-    <T extends {} = Record<string, any>>(): Storage<T> => mixins.reduceRight(
-        (storage, mixin) => mixin(storage),
-        storage,
-    ) as Storage<T>;
+export const applyStorageMixins =
+    <T extends {} = Record<string, any>>(
+        mixins: BaseStorageMixin[],
+        storage: BaseStorage,
+    ): Storage<T> =>
+        mixins.reduceRight(
+            (storage, mixin) => mixin(storage),
+            storage,
+        ) as Storage<T>;
