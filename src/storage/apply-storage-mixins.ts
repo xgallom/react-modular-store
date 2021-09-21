a91d23554a5ab222f6fb69705dffc1aa7a3b2a0b
Milan Gallo <gallo.milan.jr@gmail.com>
2021-09-21 12:20:52 +0200

    Lets just reduce left
    Signed-off-by: Milan Gallo <gallo.milan.jr@gmail.com>

--- src/storage/apply-storage-mixins.ts
+++ src/storage/apply-storage-mixins.ts
@@ -1,7 +1,7 @@
 import {BaseStorage, BaseStorageMixin, Storage} from './interface';
 
 export const applyStorageMixins = (mixins: BaseStorageMixin[], storage: BaseStorage) =>
-    <T extends {} = Record<string, any>>(): Storage<T> => mixins.reduceRight(
+    <T extends {} = Record<string, any>>(): Storage<T> => mixins.reduce(
         (storage, mixin) => mixin(storage),
         storage,
     ) as Storage<T>;
